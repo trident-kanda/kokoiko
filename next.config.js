04135e8b6cfba764c69861a57d51debcbfa92bda
trident-kanda <tridentkandayuki@gmail.com>
2021-04-18 21:40:00 +0900

    APIKEY

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   env: {
     SUPABASE_URL: "https://wkfpjoyczpxgmshcxkux.supabase.co",
     SUPABASE_KEY:
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoic2VydmljZV9yb2xlIiwiaWF0IjoxNjE3OTYwMDA5LCJleHAiOjE5MzM1MzYwMDl9.LVzD1r5b7V8_81iZ4Wzg8B1FzU2l0_GYWNv-3_W3EQw",
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYxNzk2MDAwOSwiZXhwIjoxOTMzNTM2MDA5fQ.ak2iHrYQdRzynA_Zj-dnlHCIUgxMXfAXX2IYqhPBuEo",
   },
 };
