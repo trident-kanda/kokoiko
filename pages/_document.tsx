84819a411ae963de5ffdd7b9df9873a27cd99613
trident-kanda <tridentkandayuki@gmail.com>
2021-04-22 11:51:49 +0900

    document

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,6 +1,14 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import type { DocumentContext, DocumentInitialProps } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
 class MyDocument extends Document {
-  render() {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const initialProps = await Document.getInitialProps(ctx);
+    return initialProps;
+  }
+  render(): JSX.Element {
     return (
       <Html lang="ja">
         <Head />
