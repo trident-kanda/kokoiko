d5fcda257768cb1ba1d2a375abcfd819970638e9
trident-kanda <tridentkandayuki@gmail.com>
2021-04-30 01:07:07 +0900

    recruitmentForm

--- src/pages/recruitment.tsx
+++ src/pages/recruitment.tsx
@@ -1,22 +1,25 @@
 import Head from "next/head";
 import Container from "../components/Container";
 import Nav from "../components/Nav";
-import Sidenav from "../components/Sidenav";
 import { useContext, useEffect, useState } from "react";
 import { UserContext } from "../../util/userContext";
 import { useRouter } from "next/router";
 import Main from "../components/Main";
 import GoogleMap from "../components/GoogleMap";
 import SearchBox from "../components/SearchBox";
+import { useForm } from "react-hook-form";
 export default function recruiment() {
+  type formProps = {};
   const { user, session } = useContext(UserContext);
   const { replace } = useRouter();
   const defaultLatLng = { lat: 35.6809591, lng: 139.7673068 };
-  const [latLng, setLatLng] = useState(defaultLatLng);
+  const [latLng, setLatLng] = useState<any>(null);
   const [map, setMap] = useState<any>(null);
   const [maps, setMaps] = useState<any>(null);
   const [marker, setMarker] = useState<any>(null);
   const [apiReady, setReady] = useState(false);
+  const peopleArray = [1, 2, 3, 4, 5];
+  const { handleSubmit, register } = useForm();
   const handleApiLoaded = ({ map, maps }: any) => {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition((postion) => {
@@ -29,6 +32,10 @@ export default function recruiment() {
     setMap(map);
     setMaps(maps);
     setReady(true);
+  };
+
+  const onSubmit = (data: formProps) => {
+    console.log(data);
   };
 
   const mapClick = ({ x, y, lat, lng, event }: any) => {
@@ -64,13 +71,50 @@ export default function recruiment() {
         <Nav />
         <Main>
           <div className=" bg-white  sm:rounded-lg shadow p-4">
+            <h2 className=" text-lg font-bold text-gray-700 ">
+              集合場所
+              <span className="ml-2 text-gray-500">タップ/クリックで選択</span>
+            </h2>
             {apiReady && <SearchBox maps={maps} map={map} />}
             <GoogleMap
-              defaultZoom={17}
+              defaultZoom={18}
               defaultCenter={defaultLatLng}
               onGoogleApiLoaded={handleApiLoaded}
               onClick={mapClick}
             ></GoogleMap>
+            <form className="" onSubmit={handleSubmit(onSubmit)}>
+              <label className="text-lg font-bold text-gray-700 ">
+                集合場所詳細
+              </label>
+              <textarea
+                {...register("detailsPlace")}
+                className="block w-full focus:outline-none focus:border-green-500 p-1 border-gray-500 border-2 rounded"
+              ></textarea>
+              <label className="text-lg font-bold text-gray-700 ">概要</label>
+              <textarea
+                {...register("overview")}
+                className="block w-full focus:outline-none focus:border-green-500 p-1 border-gray-500 border-2 rounded"
+              ></textarea>
+              <label className="text-lg font-bold text-gray-700 ">
+                募集人数
+              </label>
+              <select
+                {...register("numberPeople")}
+                className="block w-full focus:outline-none focus:border-green-500 p-1 border-gray-500 border-2 rounded"
+              >
+                {peopleArray.map((number) => {
+                  return (
+                    <option key={number} value={number}>
+                      {number}人
+                    </option>
+                  );
+                })}
+              </select>
+              <div className="h-4" />
+              <button className="block w-full focus:outline-none  rounded bg-green-500 text-white py-2 ">
+                投稿
+              </button>
+            </form>
           </div>
         </Main>
       </Container>
