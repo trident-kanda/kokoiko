015dbfb5ed7a3beb0878ceab44e42413d664b223
trident-kanda <tridentkandayuki@gmail.com>
2021-05-17 22:32:24 +0900

    modalStyle

--- src/pages/friendsearch.tsx
+++ src/pages/friendsearch.tsx
@@ -4,8 +4,10 @@ import Container from "../components/Container";
 import Link from "next/link";
 import { useState } from "react";
 import { useMutation, gql } from "@apollo/client";
+import Modal from "react-modal";
 const friendsearch = ({ user }: any) => {
   const [inputid, setId] = useState("");
+  const [modalState, modalChange] = useState(false);
   const SEND_FRIEND = gql`
     mutation ($uid: uuid!, $name: String!) {
       update_users(_set: { name: $name }, where: { uid: { _eq: $uid } }) {
@@ -15,13 +17,53 @@ const friendsearch = ({ user }: any) => {
       }
     }
   `;
-  const [nameChangeMutation] = useMutation(SEND_FRIEND);
+  const [sendFriendMutation] = useMutation(SEND_FRIEND);
+  Modal.setAppElement("#__next");
+  const closeModal = () => {
+    modalChange(false);
+  };
+
+  const customStyles: any = {
+    overlay: {
+      position: "fixed",
+      top: 0,
+      left: 0,
+      backgroundColor: "rgba(0,0,0,0.3)",
+    },
+
+    content: {
+      top: "30%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      width: "500px",
+      height: "300px",
+      transform: "translate(-50%, -50%)",
+    },
+  };
+
   return (
     <Container>
       <Link href="/userpage">
         <a className="hover:text-gray-500">戻る</a>
       </Link>
       <div className="bg-white shadow-sm sm:rounded-lg pt-5 px-10 pb-10  ">
+        <Modal
+          isOpen={modalState}
+          onRequestClose={closeModal}
+          style={customStyles}
+        >
+          <div>
+            <button
+              onClick={() => {
+                modalChange(false);
+              }}
+            >
+              閉じる
+            </button>
+          </div>
+        </Modal>
         <h2 className="text-xl bold text-gray-500 pb-1">ユーザー検索</h2>
         <div className="flex items-center">
           <div className=" w-5/6 ">
@@ -35,7 +77,9 @@ const friendsearch = ({ user }: any) => {
           <div className=" w-1/6">
             <button
               className="ml-2 py-2 bg-green-500 rounded-lg hover:bg-green-300 w-full text-white focus:outline-none"
-              onClick={() => {}}
+              onClick={() => {
+                modalChange(true);
+              }}
             >
               検索
             </button>
