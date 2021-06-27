2940fc6a361c6c209d754c136d52507670ed6b5e
trident-kanda <tridentkandayuki@gmail.com>
2021-06-27 17:03:59 +0900

    displayData

---
+++ src/components/DisplayData.tsx
@@ -0,0 +1,13 @@
+type props = {
+  title: string;
+};
+const DisplayData = ({ title }: any) => {
+  return (
+    <div className="w-full sm:w-1/2 p-2">
+      <h2 className="text-xl bg-green-300 py-1  text-white text-center">
+        {title}
+      </h2>
+    </div>
+  );
+};
+export default DisplayData;
